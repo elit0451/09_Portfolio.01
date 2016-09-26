8d4153e9a95ac97ea82e12772fa513ffb34e79ac
Elitsa Marinovska <Elitsa Marinovska@DESKTOP-03FMA85>
2016-09-26 14:39:55 +0200

    Add project files.

---
+++ 09_Portfolio/Stock.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _09_Portfolio
+{
+    internal class Stock
+    {
+        public Stock()
+        {
+        }
+
+        public Stock(string symbol, double pricePerShare, int numShares)
+        {
+            Symbol = symbol;
+            PricePerShare = pricePerShare;
+            NumShares = numShares;
+        }
+
+        public int NumShares { get; internal set; }
+        public double PricePerShare { get; internal set; }
+        public string Symbol { get; internal set; }
+
+        internal double GetValue()
+        {
+            return PricePerShare * NumShares;
+        }
+
+        internal static double TotalValue(Stock[] stocks)
+        {
+            return stocks[0].GetValue() + stocks[1].GetValue();
+        }
+        public override string ToString()
+        {
+            return "Stock[symbol=" + Symbol+ ",pricePerShare="+ PricePerShare+",numShares="+ NumShares+"]";
+        }
+        public override bool Equals(object obj)
+        {
+            Stock abc = (Stock)obj;
+            return (this.Symbol == abc.Symbol && this.PricePerShare == abc.PricePerShare && this.NumShares == abc.NumShares);
+        }
+    }
+}
